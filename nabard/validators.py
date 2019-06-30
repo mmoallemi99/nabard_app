49ebb3671d84153d93e0dae92f465054b17ec1ed
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-06-30 20:41:08 +0430

    added archive formats to powerpoint

--- nabard/validators.py
+++ nabard/validators.py
@@ -4,7 +4,7 @@ def validate_powerpoint_file(value):
     from django.core.exceptions import ValidationError
 
     extension = os.path.splitext(value.name)[1]
-    valid_extensions = ['.ppt', '.pptx', ]
+    valid_extensions = ['.ppt', '.pptx', '.zip', '.rar', '.7z', '.tar.gz', ]
     if not extension.lower() in valid_extensions:
         raise ValidationError('پسوند فایل اشتباه است')
 
