d27e33587b9acbc81342d3b15810841f5e140b6f
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-06-30 12:04:37 +0430

    added postgresql settings

--- nabard_app/settings.py
+++ nabard_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nabard.apps.NabardConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'nabard_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nabard',
+        'USER': 'nabard',
+        'PASSWORD': '1234',
+        'PORT': 5432,
     }
 }
 
