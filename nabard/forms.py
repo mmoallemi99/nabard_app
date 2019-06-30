24a4c6b86e34777655629be407b08152b62cdf0f
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-06-30 19:14:55 +0430

    registration form works fine (almost) field name aro khali bedan chizi nemire too db faghat project o file ash

--- nabard/forms.py
+++ nabard/forms.py
@@ -4,10 +4,18 @@ from .models import Team, TeamMember
 
 
 class TeamRegistrationForm(forms.ModelForm):
-
     class Meta:
         model = Team
         fields = '__all__'
+
+        APP_TITLE_WIDGET = forms.TextInput(
+            attrs={
+                'placeholder': 'لطفا عنوان طرح خود را وارد کنید',
+                'dir': 'rtl',
+            })
+        widgets = {
+            'app_title': APP_TITLE_WIDGET,
+        }
 
 
 MEMBER_FIELD_WIDGET = forms.TextInput(
@@ -23,3 +31,4 @@ MEMBERS_FORMSET = {
 MembersFormSet = forms.inlineformset_factory(Team, TeamMember, fields=('full_name',),
                                              extra=5, can_delete=False,
                                              widgets=MEMBERS_FORMSET)
+
--- nabard/views.py
+++ nabard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+from django import forms
+
 from .forms import TeamRegistrationForm, MembersFormSet
 
 
@@ -19,6 +21,8 @@ def team_register(request):
         if members_formset.is_valid() and team_form.is_valid():
             new_team = team_form.save()
             new_members = members_formset.save(commit=False)
+            if not new_members:
+                raise forms.ValidationError("باید حداقل نام یک عضو را وارد کنید")
             for member in new_members:
                 member.team = new_team
                 member.save()
@@ -26,6 +30,8 @@ def team_register(request):
     else:
         team_form = TeamRegistrationForm()
         members_formset = MembersFormSet()
+
+    members_formset[0].use_required_attribute = True
 
     context = {
         'members_formset': members_formset,
