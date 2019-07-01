c6a371ff5f7f6cfbe3f76154be8e829b05b28cf6
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-07-01 22:05:02 +0430

    validators error edited

--- nabard/validators.py
+++ nabard/validators.py
@@ -1,16 +1,16 @@
 def validate_powerpoint_file(value):
-    print(value)
     import os
     from django.core.exceptions import ValidationError
 
     extension = os.path.splitext(value.name)[1]
     valid_extensions = ['.ppt', '.pptx', '.zip', '.rar', '.7z', '.tar.gz', ]
     if not extension.lower() in valid_extensions:
-        raise ValidationError('پسوند فایل اشتباه است')
+        valid_str = " , ".join(valid_extensions)
+        error_msg = 'پسوند فایل اشتباه است - پسوند های مجاز: {0}'.format(valid_str)
+        raise ValidationError(error_msg)
 
 
 def validate_archived_file(value):
-    print(value)
     import os
     from django.core.exceptions import ValidationError
 
@@ -18,4 +18,6 @@ def validate_archived_file(value):
     valid_extensions = ['.zip', '.rar', '.7z', '.tar.gz', ]
 
     if extension not in valid_extensions:
-        raise ValidationError('پسوند فایل اشتباه است')
+        valid_str = " , ".join(valid_extensions)
+        error_msg = 'پسوند فایل اشتباه است - پسوند های مجاز: {0}'.format(valid_str)
+        raise ValidationError(error_msg)
