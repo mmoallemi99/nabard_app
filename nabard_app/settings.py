99d4eefca5a42b6b715e5d8e9ce3e1a3e7e2f81a
mmoallemi99 <mohammadmoallemi@outlook.com>
2019-07-01 16:10:30 -0400

    debug set to false

--- nabard_app/settings.py
+++ nabard_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w@6-!+=y&v_-ij8z1ex=r1o%^a0h)au0ew4yrplllqk0^8p9$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['94.184.89.123']
 
--- nabard_app/urls.py
+++ nabard_app/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('modir/', admin.site.urls),
     path('', include('nabard.urls', namespace='nabard')),
 ]
 
