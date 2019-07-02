d041a7fd5bdf85e762044285c0d7012cf303c5f3
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-07-02 20:01:17 +0430

    added more front things including me lol

--- nabard_app/settings.py
+++ nabard_app/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w@6-!+=y&v_-ij8z1ex=r1o%^a0h)au0ew4yrplllqk0^8p9$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['94.184.89.123']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['94.184.89.123']
 
 
 # Application definition
